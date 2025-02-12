436c97cd05307822ace15d0960d1f64cf8635a5e
Kenneth Gorking <kgorking@outlook.com>
2025-02-12 01:04:10 +0100

    Fixed naming

--- scripts/main_test.gd
+++ scripts/main_test.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-const NestedTypesArray = preload("res://scripts/typed_array.gd")
+const TypedArray = preload("res://scripts/typed_array.gd")
 const ArrayMap = preload("res://scripts/array_map.gd")
 
 func _init() -> void:
-	var array_test := NestedTypesArray.new()
+	var array_test := TypedArray.new()
 	array_test.test_run()
 	
 	var array_map_test := ArrayMap.new()
