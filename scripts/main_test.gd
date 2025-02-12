1e11a0f87e8e8da49ed4dcf8dddc55894839181c
Kenneth Gorking <kgorking@outlook.com>
2025-02-12 11:37:46 +0100

    Remove old file

--- scripts/main_test.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-const DeduceArray = preload("res://scripts/deduce_array.gd")
-const TypedArrayMap = preload("res://scripts/typed_array_map.gd")
-
-
-func _init() -> void:
-	DeduceArray.new().test_run()
-	TypedArrayMap.new().test_run()
